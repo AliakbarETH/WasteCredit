8bd66a47710f184ca6ea01a615a7b6d7a642570e
ALYAN ALAM <alyanalam780@gmail.com>
2023-09-04 19:29:31 +0500

    User DB Schema

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", userAuthentication);
-app.use("/users", userAuthentication);
+// app.use("/", userAuthentication);
+app.use("/user", userAuthentication);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
