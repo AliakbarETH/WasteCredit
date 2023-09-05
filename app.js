a8a8f7598634a166d1c6ae76a9a83a72dcca52b9
ALYAN ALAM <alyanalam780@gmail.com>
2023-09-05 22:18:55 +0500

    fix IV

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // app.use("/", userAuthentication);
 app.use("/user", userAuthentication);
-app.use("/sendotp", otpRoutess);
+app.use("/sendotp", otpRoutes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
