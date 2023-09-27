a09bf59aae0583d00dc2b0cfac337bb44453d6dc
ALYAN ALAM <alyanalam780@gmail.com>
2023-09-27 18:16:30 +0500

    Store Apis

--- controller/storeController.js
+++ controller/storeController.js
@@ -3,13 +3,6 @@ const {
   storeRegisterSchema,
 } = require("./validationSchema/registerStoreSchema");
 module.exports = {
-  getAllStores: async function (req, res) {
-    try {
-    } catch (error) {
-      const response = { Status: "Failure", Details: error.message };
-      return res.status(400).send(response);
-    }
-  },
   createStore: async function (req, res) {
     try {
       const { error } = storeRegisterSchema.validate(req.body);
