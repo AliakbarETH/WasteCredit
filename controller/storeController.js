938a48575c47d0e1c40247b033ebe05c7f8dbf27
ALYAN ALAM <alyanalam780@gmail.com>
2023-09-27 17:52:31 +0500

    create delete store controller

--- controller/storeController.js
+++ controller/storeController.js
@@ -51,9 +51,9 @@ module.exports = {
   },
   deleteStore: async function (req, res) {
     try {
-      const store_id = req.body.store_id;
-      const store_name = req.body.store_id;
-      res.send(store_id, store_name);
+      const id = req.body.id;
+      const response = await storeService.deleteStore(id);
+      res.send(response);
     } catch (error) {
       const response = { Status: "Failure", Details: error.message };
       return res.status(400).send(response);
