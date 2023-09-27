f62e986ab8b0fd7ec67d0139ba32fa601f4a1d5b
ALYAN ALAM <alyanalam780@gmail.com>
2023-09-27 16:28:37 +0500

    Add more Controller

--- controller/storeController.js
+++ controller/storeController.js
@@ -1,5 +1,5 @@
 module.exports = {
-  getStore: async function (req, res) {
+  getAllStores: async function (req, res) {
     try {
       const store_id = req.body.store_id;
       res.send(store_id);
@@ -8,7 +8,37 @@ module.exports = {
       return res.status(400).send(response);
     }
   },
-  setStore: async function (req, res) {
+  createStore: async function (req, res) {
+    try {
+        const store_id = req.body.store_id;
+        const store_name =req.body.store_id;
+      res.send(store_id,store_name );
+    } catch (error) {
+      const response = { Status: "Failure", Details: error.message };
+      return res.status(400).send(response);
+    }
+  },
+  getStoreById: async function (req, res) {
+    try {
+        const store_id = req.body.store_id;
+        const store_name =req.body.store_id;
+      res.send(store_id,store_name );
+    } catch (error) {
+      const response = { Status: "Failure", Details: error.message };
+      return res.status(400).send(response);
+    }
+  },
+  updateStore: async function (req, res) {
+    try {
+        const store_id = req.body.store_id;
+        const store_name =req.body.store_id;
+      res.send(store_id,store_name );
+    } catch (error) {
+      const response = { Status: "Failure", Details: error.message };
+      return res.status(400).send(response);
+    }
+  },
+  deleteStore: async function (req, res) {
     try {
         const store_id = req.body.store_id;
         const store_name =req.body.store_id;
