de1914af26e5f544f444102bc64a3b70d89f201f
ALYAN ALAM <alyanalam780@gmail.com>
2023-09-29 21:55:40 +0500

    fix bugs

--- routes/storeRoutes.js
+++ routes/storeRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const storeController = require("../controller/storeController");
 
-router.get("/", storeController.getAllStores);
+router.post("/", storeController.getAllStores);
 router.post("/createStore", storeController.createStore);
 router.post("/getStoreById", storeController.getStoreById);
 router.put("/updateStore", storeController.updateStore);
